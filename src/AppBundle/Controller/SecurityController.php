7729ae8c0958c8aaf3f4e8ec4df0103673b5cb6c
Alexey Popov <alexey.popov@umbrella-web.com>
2017-08-03 22:27:52 +0300

    created redirects login and register to product_list if user is auth

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,17 +12,23 @@ class SecurityController extends Controller {
      * @Route("/login", name="security_login")
      */
     public function loginAction() {
-        $authenticationUtils = $this->get('security.authentication_utils');
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('product_list', array(), 301);
+        } else {
+            $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
 
-        $form = $this->createForm(LoginForm::class, ['_username' => $lastUsername,]);
+            $form = $this->createForm(LoginForm::class, ['_username' => $lastUsername,]);
 
-        return $this->render('security/login.html.twig', array('form' => $form->createView(), 'error' => $error,));
+            return $this->render('security/login.html.twig', array('form' => $form->createView(), 'error' => $error,));
+
+        }
     }
 
     /**
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserRegistrationForm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,31 +15,30 @@ class UserController extends Controller {
      * @Route("/register", name="user_register")
      */
     public function registerAction(Request $request) {
-        $form = $this->createForm(UserRegistrationForm::class);
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('product_list', array(), 301);
+        } else {
+            $form = $this->createForm(UserRegistrationForm::class);
 
-        $form->handleRequest($request);
+            $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            /**
-             * @var User $user
-             */
-            $user = $form->getData();
+            if ($form->isValid()) {
+                /**
+                 * @var User $user
+                 */
+                $user = $form->getData();
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-            $this->addFlash('success', 'Welcome ' . $user->getEmail());
+                $this->addFlash('success', 'Welcome ' . $user->getEmail());
 
-            return $this->get('security.authentication.guard_handler')
-                ->authenticateUserAndHandleSuccess(
-                    $user,
-                    $request,
-                    $this->get('app.security.login_form_authenticator'),
-                    'main'
-                );
+                return $this->get('security.authentication.guard_handler')->authenticateUserAndHandleSuccess($user, $request, $this->get('app.security.login_form_authenticator'), 'main');
+            }
+
+            return $this->render('user/register.html.twig', ['form' => $form->createView(),]);
         }
-
-        return $this->render('user/register.html.twig', ['form' => $form->createView(),]);
     }
 }
